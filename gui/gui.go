7a025ba2a38ff8beb83e5bf1d048313de276152a
Gyro7 <dragofuoco2004@gmail.com>
2021-01-29 16:10:13 +0100

    added some lines and a spacer

--- gui/gui.go
+++ gui/gui.go
@@ -9,6 +9,7 @@ import (
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/cmd/fyne_settings/settings"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
 )
 
 //Call calls the main gui application
@@ -16,14 +17,10 @@ func Call() {
 	a := app.New()
 	w := a.NewWindow("Osiris Password Manager")
 	w.SetTitle("Osiris Password Manager")
-	//title := widget.NewLabelWithStyle("Hello Fyne!", fyne.TextAlignCenter, fyne.TextStyle{})
-	title := canvas.NewText("Osiris Password Manager", color.RGBA{R: uint8(128), G: uint8(0), B: uint8(128), A: uint8(1)})
-	title.TextSize = 34
-	title.TextStyle.Bold = true
-	title.Alignment = fyne.TextAlignCenter
-	description := canvas.NewText("Questo è come funziona il porcodio di password manager blablabla", color.RGBA{R: uint8(80), G: uint8(0), B: uint8(128), A: uint8(1)})
-	description.TextSize = 20
-	description.Alignment = fyne.TextAlignCenter
+
+	//usable variables
+	spacer := layout.NewSpacer()
+	spacer.Move(fyne.NewPos(1, 1))
 
 	//menu with settings, edit and help
 	settingsItem := fyne.NewMenuItem("Settings", func() {
@@ -49,20 +46,20 @@ func Call() {
 	})
 	helpMenu := fyne.NewMenu("Help",
 		fyne.NewMenuItem("Documentation", func() {
-			u, _ := url.Parse("https://developer.fyne.io")
+			u, _ := url.Parse("https://github.com/Gyro7/Osiris-pwm")
 			_ = a.OpenURL(u)
 		}),
-		fyne.NewMenuItem("Support", func() {
-			u, _ := url.Parse("https://fyne.io/support/")
+		fyne.NewMenuItem("Request feature", func() {
+			u, _ := url.Parse("https://github.com/Gyro7/Osiris-pwm/pulls")
 			_ = a.OpenURL(u)
 		}),
 		fyne.NewMenuItemSeparator(),
-		fyne.NewMenuItem("Sponsor", func() {
-			u, _ := url.Parse("https://github.com/sponsors/fyne-io")
+		fyne.NewMenuItem("Report a bug", func() {
+			u, _ := url.Parse("https://github.com/Gyro7/Osiris-pwm/issues/new")
 			_ = a.OpenURL(u)
 		}))
 	mainMenu := fyne.NewMainMenu(
-		// a quit item will be appended to our first menu
+		// a quit item will be appended to the first menu
 		fyne.NewMenu("File", fyne.NewMenuItemSeparator(), settingsItem),
 		fyne.NewMenu("Edit", cutItem, copyItem, pasteItem, fyne.NewMenuItemSeparator()),
 		helpMenu,
@@ -70,8 +67,17 @@ func Call() {
 	w.SetMainMenu(mainMenu)
 	w.SetMaster()
 
+	//title := widget.NewLabelWithStyle("Hello Fyne!", fyne.TextAlignCenter, fyne.TextStyle{})
+	title := canvas.NewText("Osiris Password Manager", color.RGBA{R: uint8(128), G: uint8(0), B: uint8(128), A: uint8(1)})
+	title.TextSize = 34
+	title.TextStyle.Bold = true
+	title.Alignment = fyne.TextAlignCenter
+	description := canvas.NewText("Questo è come funziona il porcodio di password manager blablabla", color.RGBA{R: uint8(80), G: uint8(0), B: uint8(128), A: uint8(1)})
+	description.TextSize = 20
+	description.Alignment = fyne.TextAlignCenter
+
 	//set content and run
-	w.SetContent(container.NewVBox(title, description))
+	w.SetContent(container.NewVBox(title, spacer, description, spacer))
 	w.ShowAndRun()
 }
 
