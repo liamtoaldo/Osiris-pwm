58d38d061f94052e318173d3f6f5fa9810846ce0
Gyro7 <dragofuoco2004@gmail.com>
2021-01-31 16:43:55 +0100

    added delete button for empty entries

--- gui/gui.go
+++ gui/gui.go
@@ -2,7 +2,7 @@ package gui
 
 import (
 	"bufio"
-	"image/color"
+	//"image/color"
 	"io/ioutil"
 	"log"
 	"net/url"
@@ -14,6 +14,7 @@ import (
 	"fyne.io/fyne/v2/cmd/fyne_settings/settings"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	//"github.com/d-tsuji/clipboard"
 )
@@ -28,9 +29,9 @@ var (
 	services  []*widget.Entry
 	usernames []*widget.Entry
 	passwords []*widget.Entry
+	grids     []*fyne.Container
 
 	isLocked bool = false
-	//TODO add delete button
 )
 
 //SetTextSize modifies the text size with a variadic function to make it easier
@@ -47,7 +48,9 @@ func Call() {
 	w.SetTitle("Osiris Password Manager")
 
 	//usable variables
-	purple := color.RGBA{R: uint8(128), G: uint8(0), B: uint8(128), A: uint8(1)}
+	//mainColor := color.RGBA{R: uint8(128), G: uint8(0), B: uint8(128), A: uint8(1)}
+	mainColor := theme.PrimaryColor()
+	mainView := container.NewVBox()
 
 	//menu with settings, edit and help
 	settingsItem := fyne.NewMenuItem("Settings", func() {
@@ -71,6 +74,13 @@ func Call() {
 			Clipboard: w.Clipboard(),
 		}, w)
 	})
+	deleteItem := fyne.NewMenuItem("Delete empties", func() {
+		for i := 0; i < len(grids); i++ {
+			if services[i].Text == "" && usernames[i].Text == "" && passwords[i].Text == "" {
+				updateView(mainView, false, i)
+			}
+		}
+	})
 	helpMenu := fyne.NewMenu("Help",
 		fyne.NewMenuItem("Documentation", func() {
 			u, _ := url.Parse("https://github.com/Gyro7/Osiris-pwm")
@@ -88,27 +98,28 @@ func Call() {
 	mainMenu := fyne.NewMainMenu(
 		// a quit item will be appended to the first menu
 		fyne.NewMenu("File", fyne.NewMenuItemSeparator(), settingsItem),
-		fyne.NewMenu("Edit", cutItem, copyItem, pasteItem, fyne.NewMenuItemSeparator()),
+		fyne.NewMenu("Edit", cutItem, copyItem, pasteItem, fyne.NewMenuItemSeparator(), deleteItem),
 		helpMenu,
 	)
 	w.SetMainMenu(mainMenu)
 	w.SetMaster()
 
 	//title and description
-	title := canvas.NewText("Osiris Password Manager", purple)
+	title := canvas.NewText("Osiris Password Manager", theme.PrimaryColor())
 	title.TextSize = 34
 	title.TextStyle.Bold = true
 	title.Alignment = fyne.TextAlignCenter
-	description := canvas.NewText("Questo è come funziona il di password manager blablabla", color.RGBA{R: uint8(80), G: uint8(0), B: uint8(128), A: uint8(1)})
+	description := canvas.NewText("Questo è come funziona il di password manager blablabla", theme.PrimaryColor())
 	description.TextSize = 22
 	description.Alignment = fyne.TextAlignCenter
+	description.TextStyle = fyne.TextStyle{Italic: true}
 
 	//table with usernames, values and passwords
-	serviceTitle := canvas.NewText("Services", purple)
+	serviceTitle := canvas.NewText("Services", mainColor)
 	serviceTitle.Alignment = fyne.TextAlignCenter
-	usernameTitle := canvas.NewText("Usernames", purple)
+	usernameTitle := canvas.NewText("Usernames", mainColor)
 	usernameTitle.Alignment = fyne.TextAlignCenter
-	passwordTitle := canvas.NewText("Passwords", purple)
+	passwordTitle := canvas.NewText("Passwords", mainColor)
 	passwordTitle.Alignment = fyne.TextAlignCenter
 	SetTextSize(20, serviceTitle, usernameTitle, passwordTitle)
 
@@ -116,7 +127,7 @@ func Call() {
 	lock := widget.NewButtonWithIcon("Block entries", fyne.NewStaticResource("lock", getIcon("gui/lock.png")), func() { handleAllLocks() })
 	legend := container.NewAdaptiveGrid(5, serviceTitle, usernameTitle, passwordTitle, lock)
 	//mainView of the app
-	mainView := container.NewVBox(
+	mainView = container.NewVBox(
 		title,
 		description,
 		layout.NewSpacer(),
@@ -130,7 +141,7 @@ func Call() {
 		usernames = append(usernames, widget.NewEntry())
 		passwords = append(passwords, widget.NewEntry())
 		w := a.NewWindow("Create new entry")
-		title := canvas.NewText("Create your new entry here", color.RGBA{R: uint8(80), G: uint8(0), B: uint8(128), A: uint8(1)})
+		title := canvas.NewText("Create your new entry here", mainColor)
 		title.TextSize = 22
 		service := widget.NewEntry()
 		service.PlaceHolder = "Insert the service here"
@@ -145,10 +156,8 @@ func Call() {
 			services[getFreePosition()-1].SetText(service.Text)
 			usernames[getFreePosition()-1].SetText(username.Text)
 			passwords[getFreePosition()-1].SetText(password.Text)
-			handleLock(services[getFreePosition()-1],
-				usernames[getFreePosition()-1], passwords[getFreePosition()-1])
-
-			updateView(mainView)
+			updateView(mainView, true, len(services)-1)
+			handleLock(services[getFreePosition()-1], usernames[getFreePosition()-1], passwords[getFreePosition()-1])
 			w.Hide()
 		})
 		w.SetContent(container.NewVBox(
@@ -191,65 +200,22 @@ func shortcutFocused(s fyne.Shortcut, w fyne.Window) {
 	}
 }
 
+//TODO
 // func retrieveData(a fyne.App, w fyne.Window) {
 // 	//for i := 0; i < len(services); i++ {
 // 	//services[i] = file.Text ...
-// 	services = append(services, widget.NewButton("Minecraft", func() {}))
-// 	usernames = append(usernames, widget.NewButton("tiziobe435", func() {}))
-// 	passwords = append(passwords, widget.NewButton("daudhoasdhd2346°ç*èòpè", func() {}))
-// 	edits = append(edits, widget.NewButton("Edit", func() {
-// 		w := a.NewWindow("Edit Entry")
-// 		title := canvas.NewText("Edit your values here", color.RGBA{R: uint8(80), G: uint8(0), B: uint8(128), A: uint8(1)})
-// 		title.TextSize = 22
-// 		service := widget.NewEntry()
-// 		service.Text = services[getFreePosition()-1].Text
-// 		service.PlaceHolder = "Insert the service here"
-// 		username := widget.NewEntry()
-// 		username.Text = usernames[getFreePosition()-1].Text
-// 		username.PlaceHolder = "Insert your username here"
-// 		password := widget.NewPasswordEntry()
-// 		password.PlaceHolder = "Insert your password here"
-// 		service.Text = services[getFreePosition()-1].Text
-
-// 		cancel := widget.NewButton("Cancel", func() {
-// 			w.Hide()
-// 		})
-// 		confirm := widget.NewButton("Confirm", func() {
-// 			services[getFreePosition()-1].SetText(service.Text)
-// 			usernames[getFreePosition()-1].SetText(username.Text)
-// 			passwords[getFreePosition()-1].SetText(password.Text)
-// 			w.Hide()
-// 		})
-// 		w.SetContent(container.NewVBox(
-// 			container.NewHBox(layout.NewSpacer(), title, layout.NewSpacer()),
-// 			layout.NewSpacer(),
-// 			service,
-// 			layout.NewSpacer(),
-// 			username,
-// 			layout.NewSpacer(),
-// 			password,
-// 			layout.NewSpacer(),
-// 			container.NewHBox(
-// 				cancel,
-// 				confirm,
-// 			),
-// 		))
-// 		w.Resize(fyne.NewSize(480, 480))
-// 		w.CenterOnScreen()
-// 		w.Show()
-// 	}))
-// 	copies = append(copies, widget.NewButton("Copy", func() {
-// 		clipboard.Set(passwords[getFreePosition()].Text)
-// 	}))
-// 	//}
-// }
 
 func getFreePosition() int {
 	return len(services)
 }
 
-func updateView(mainView *fyne.Container) {
-	mainView.Add(container.NewAdaptiveGrid(5, services[len(services)-1], usernames[len(services)-1], passwords[len(services)-1]))
+func updateView(mainView *fyne.Container, add bool, i int) {
+	if add {
+		grids = append(grids, container.NewAdaptiveGrid(5, services[i], usernames[i], passwords[i]))
+		mainView.Add(grids[i])
+	} else {
+		mainView.Remove(grids[i])
+	}
 
 }
 
@@ -303,4 +269,11 @@ func handleLock(service *widget.Entry, username *widget.Entry, password *widget.
 	service.Enable()
 	username.Enable()
 	password.Enable()
+}
+
+func remove(slice []*fyne.Container, s int) []*fyne.Container {
+	if len(slice) > 0 {
+		slice = slice[:len(slice)-1]
+	}
+	return slice
 }
