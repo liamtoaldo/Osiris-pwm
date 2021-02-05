93727660daff509219af0c30d251b2d997b4fb12
Gyro7 <dragofuoco2004@gmail.com>
2021-02-05 22:30:46 +0100

    added two functions to save the main data to files

--- crypt/crypt.go
+++ crypt/crypt.go
@@ -8,6 +8,7 @@ import (
 	"io"
 	"io/ioutil"
 	"math/rand"
+	"os"
 	"time"
 )
 
@@ -49,7 +50,6 @@ func EncryptStringInFile(key []byte, text string, file string) {
 		fmt.Println(err)
 	}
 
-	//seal the new encrypted string in the file
 	err = ioutil.WriteFile(file, gcm.Seal(nonce, nonce, textInBytes, nil), 0777)
 	if err != nil {
 		fmt.Println(err)
@@ -89,3 +89,79 @@ func DecryptStringFromFile(key []byte, file string) string {
 
 //GetGlobalKey returns the global key that encrypts and decrypts the global
 func GetGlobalKey() []byte { return []byte("q1ozLRcb3YrlpvxZqUE7LviP5MFwnT5w") }
+
+//EncryptDataStringInFile encrypts a string given the key and the file to a data file (one containing services, usernames and passwords.)
+func EncryptDataStringInFile(key []byte, text string, file string) {
+	textInBytes := []byte(text)
+
+	//generates a new aes cipher using our 32 byte long key
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//gcm is a mode of operation for symmetric key cryptographic block ciphers
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	//random sequence
+	if _, err = io.ReadFull(crand.Reader, nonce); err != nil {
+		fmt.Println(err)
+	}
+
+	//seal the new encrypted string in the file
+	for i := 0; i < 999; i++ {
+		file += fmt.Sprint(i)
+		println(file)
+		if _, err := os.Stat(file); os.IsNotExist(err) {
+			break
+		}
+	}
+
+	err = ioutil.WriteFile(file, gcm.Seal(nonce, nonce, textInBytes, nil), 0777)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+//DecryptDataStringFromFile decrypts a string given the key and the file with DATA
+func DecryptDataStringFromFile(key []byte, file string) string {
+
+	ciphertext, err := ioutil.ReadFile(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	gcm, err := cipher.NewGCM(c)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	nonceSize := gcm.NonceSize()
+	if len(ciphertext) < nonceSize {
+		fmt.Println(err)
+	}
+
+	for i := 0; i < 999; i++ {
+		file += fmt.Sprint(i)
+		if _, err := os.Stat(file); os.IsNotExist(err) {
+			break
+		}
+	}
+
+	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
+	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return string(plaintext)
+}
