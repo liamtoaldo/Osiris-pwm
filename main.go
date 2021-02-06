a6bb33d4a90037cb7978e40abb2b36c6bde535da
Gyro7 <65873913+Gyro7@users.noreply.github.com>
2021-02-07 00:08:32 +0100

    Update main.go

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"Osiris-pwm/gui"
 	_ "fmt"
+	"os"
 )
 
 func main() {
+	//create the data and the gui folders if they don't already exist
+	os.Mkdir("data", 0777)
+	os.Mkdir("gui", 0777)
 	//call the main gui and run it
 	gui.Call()
 }
-
